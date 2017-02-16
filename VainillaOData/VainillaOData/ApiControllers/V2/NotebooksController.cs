ef1272a7760347c120c855bc9b1ddab7fb0f1de9
Jorge <jogonzal@microsoft.com>
2017-02-16 11:15:56 -0800

    Duplicated controllers for different versions - now throwing 500

---
+++ VainillaOData/VainillaOData/ApiControllers/V2/NotebooksController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+using System.Web.OData.Routing;
+
+namespace VainillaOData.ApiControllers.V2
+{
+	public class NotebooksController : ApiController
+	{
+		[ODataRoute("Notebooks")]
+		public IHttpActionResult Get()
+		{
+			return Ok(new
+			{
+				name = "Jorge"
+			});
+		}
+	}
+}
