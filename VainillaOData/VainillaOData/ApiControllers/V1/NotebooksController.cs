45d7d14090e8bc41e02742de2ef655d7ca7207cf
Jorge <jogonzal@microsoft.com>
2017-02-16 11:11:29 -0800

    Now working with attribute routing

--- VainillaOData/VainillaOData/ApiControllers/V1/NotebooksController.cs
+++ VainillaOData/VainillaOData/ApiControllers/V1/NotebooksController.cs
@@ -1,9 +1,11 @@
 ﻿using System.Web.Http;
+using System.Web.OData.Routing;
 
-namespace VainillaOData.ApiControllers
+namespace VainillaOData.ApiControllers.V1
 {
 	public class NotebooksController : ApiController
 	{
+		[ODataRoute("Notebooks")]
 		public IHttpActionResult Get()
 		{
 			return Ok(new
